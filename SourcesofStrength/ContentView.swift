0125e404647fbe398fbf682342cb1d0f741a5763
Greg Schloemer <gregs2011@gmail.com>
2020-04-07 13:15:10 -0400

    Initial Commit

---
+++ SourcesofStrength/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SourcesofStrength
+//
+//  Created by Greg Schloemer on 4/7/20.
+//  Copyright © 2020 Berea Community Schools. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
